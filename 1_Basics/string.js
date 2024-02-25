5c617e5f7af0f42e4e33318a6b6309f317b7130b
siddgeek <91756731+siddgeek@users.noreply.github.com>
2024-02-25 13:13:48 +0000

    string commit

---
+++ 1_Basics/string.js
@@ -0,0 +1,9 @@
+let firstName = "Siddharth"
+console.log(firstName);
+firstName = "Mohit"
+console.log(firstName);
+firstName = firstName.toUpperCase()
+console.log(firstName);
+firstName = firstName.toLowerCase()
+console.log(firstName);
+console.log(firstName.length)
