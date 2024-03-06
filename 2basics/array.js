2c7e4b9a335833e6d73aff55e70800a2bc23dd12
Siddharth Gupta <91756731+siddgeek@users.noreply.github.com>
2024-03-06 18:13:35 +0000

    new array

--- 2basics/array.js
+++ 2basics/array.js
@@ -1,6 +1,5 @@
 // array
-const array23 =['sd', 'fg', 'gf']
-console.log(array23[1]);
+
 const myArr = [0, 1, 2, 3, 4, 5]
 const myHeors = ["shaktiman", "naagraj"]
 
@@ -38,4 +37,3 @@ console.log("B ", myArr);
 const myn2 = myArr.splice(1, 3)
 console.log("C ", myArr);
 console.log(myn2);
-console.log("array");
