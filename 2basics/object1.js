eac865a855d9ead223477f6a40e38e01f521d190
Siddharth Gupta <91756731+siddgeek@users.noreply.github.com>
2024-03-08 17:28:17 +0000

    how to use symbol as key in objects, object freezing, use of this through string manipulation.

--- 2basics/object1.js
+++ 2basics/object1.js
@@ -1,17 +1,34 @@
+const mysym = Symbol("Key1");
+const mysym1 = Symbol("Key2");
+
 const JsUser = {
     name: "Siddharth Gupta",
     age: 21,
     UID: "21BCS5867",
     email: "sidddh26@gmail.com",
     isLoggedIn: false,
-    lastLoginDay: ["Monday","Saturday"],
+    lastLoginDay: ["Monday", "Saturday"],
     "address": "Sushil Studio, Khutar Road, Kheri",
-    "Basics need": "Employement"
+    "Basics need": "Employment",
+    [mysym]: "Key1",
+    mysym1: "key2"
+};
+
+// console.log(JsUser.UID);
+// console.log(JsUser.name);
+// console.log(JsUser.email);
+// console.log(JsUser.age);
+// console.log(JsUser.address);
+// console.log(JsUser["address"]);
+// console.log(JsUser["Basics need"]);
+// console.log(typeof JsUser.mysym);
+// console.log(typeof JsUser.mysym1);
+JsUser.greeting = function () {
+    console.log("hey there");
 }
-console.log(JsUser.UID);
-console.log(JsUser.name);
-console.log(JsUser.email);
-console.log(JsUser.age);
-console.log(JsUser.address);
-console.log(JsUser["address"]);
-console.log(JsUser["Basics need"]);
+JsUser.greeting1 = function () {
+    console.log(`hey there, ${this.name}`);
+}
+console.log(JsUser.greeting);
+console.log(JsUser.greeting());
+console.log(JsUser.greeting1());
