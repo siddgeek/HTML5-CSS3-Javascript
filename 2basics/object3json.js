32f45c5561e0f133b664702ddd6784e3d4d726d0
Siddharth Gupta <91756731+siddgeek@users.noreply.github.com>
2024-03-09 07:56:28 +0000

    json breified

---
+++ 2basics/object3json.js
@@ -0,0 +1,20 @@
+const course = {
+    courseName: "DabDab",
+    iD: "#149",
+    price: "999"
+}
+
+console.log(course.iD);
+
+const {courseName} = course
+const {price: daam } = course
+
+console.log(courseName);
+console.log(course.price);
+console.log(daam);
+
+{
+    "name": "siddharth",
+    "id": "free",
+    "course": "CSE"
+}
