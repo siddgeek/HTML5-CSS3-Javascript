d8cf7669e8977d30cf6bfef2691ec6b5c3c5d0a4
Siddharth Gupta <91756731+siddgeek@users.noreply.github.com>
2024-03-12 15:35:42 +0000

    Immediately Invoked function expression ( ) ( ) ;

---
+++ 3basics/functions6IIFE.js
@@ -0,0 +1,10 @@
+// function sidd() {
+//     console.log("DataBase Connected");
+// }
+// sidd()
+
+// now we'll use IIFE 
+(function sidd() {
+    console.log("DataBase Connected");
+})();
+//we use ; because function can be invoked easily but it doesn't where to stop the context.
