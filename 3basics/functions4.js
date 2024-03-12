e8b98a3b54856d424ded4ff8679818c9cf5eb75c
Siddharth Gupta <91756731+siddgeek@users.noreply.github.com>
2024-03-12 02:54:51 +0000

    scope level through if else and mini hoisting

---
+++ 3basics/functions4.js
@@ -0,0 +1,43 @@
+function one(){
+    const username = "hitesh"
+
+    function two(){
+        const website = "youtube"
+        console.log(username);
+    }
+    // console.log(website);
+
+     two()
+
+}
+
+// one()
+
+if (true) {
+    const username = "hitesh"
+    if (username === "hitesh") {
+        const website = " youtube"
+        // console.log(username + website);
+    }
+    // console.log(website);
+}
+
+// console.log(username);
+
+
+// ++++++++++++++++++ interesting ++++++++++++++++++
+
+
+console.log(addone(5))
+
+function addone(num){
+    return num + 1
+}
+
+
+
+// addTwo(5) mini hoisting.
+const addTwo = function(num){
+    return num + 2
+}
+console.log(addTwo(5));
