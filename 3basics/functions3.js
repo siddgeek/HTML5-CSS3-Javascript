b9f06b27960622ba9c6734425886f821256179f9
Siddharth Gupta <91756731+siddgeek@users.noreply.github.com>
2024-03-12 02:40:28 +0000

    global and local/block scope

---
+++ 3basics/functions3.js
@@ -0,0 +1,13 @@
+var c = 300;
+let a = 300
+if( true ){
+    let a = 12
+    const b = 400
+    var c = 30
+    console.log("inner:",a);
+}
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+console.log(a);
