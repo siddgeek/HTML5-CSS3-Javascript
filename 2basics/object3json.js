291cfe7b2bff5b96d690cb5c80afb8b0ffbeddaa
Siddharth Gupta <91756731+siddgeek@users.noreply.github.com>
2024-03-13 17:54:10 +0000

    json

--- 2basics/object3json.js
+++ 2basics/object3json.js
@@ -1,20 +1,24 @@
 const course = {
     courseName: "DabDab",
     iD: "#149",
-    price: "999"
+    price: "999",
+    address: "lakhimpur"
 }
 
-console.log(course.iD);
+console.log(course.address);
 
 const {courseName} = course
 const {price: daam } = course
+const {address: gola} = course
 
 console.log(courseName);
 console.log(course.price);
 console.log(daam);
+console.log(course.iD);
 
 // {
 //     "name": "siddharth",
 //     "id": "free",
 //     "course": "CSE"
 console.log(courseName.price);
+console.log(courseName.iD);
