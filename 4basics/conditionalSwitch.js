2c6fc990557532e00068c734a99a2d2a5cce15c4
Siddharth Gupta <91756731+siddgeek@users.noreply.github.com>
2024-03-17 13:22:16 +0000

    switch

---
+++ 4basics/conditionalSwitch.js
@@ -0,0 +1,24 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+const Month = 3
+switch (Month) {
+    case 1:
+        console.log("Janauary");
+        break;
+    case 2:
+        console.log("February");
+        break;
+    case 3:
+        console.log("March");
+        break;
+        
+    default:
+        console.log("Invalid Month");
+        break;
+}
